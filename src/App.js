43cb0a8ea566545ed2f2ee691302293667d5d422
Oluwapelumi <fafyeepelumi04@gmail.com>
2022-02-04 05:59:19 -0800

    latest abyl commit

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
+import Search from './Search';
 
 function App() {
   return (
     <Router>
       <Navbar />
+      <Search />
       <Switch>
         <Route path='/' />
       </Switch>
